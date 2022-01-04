b2dd4c47621fd3469f01a6852c59f5fbafce41d9
VladimirStavinchuk <75833690+VladimirStavinchuk@users.noreply.github.com>
2022-01-04 21:19:04 +0300

    feat: completed the task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+  let result = '';
+  n < 0 ? (n = n * -1) : n;
+  n = n + '';
+
+  for (let i = n.length - 1; i >= 0; i--) {
+    result += n[i];
+  }
+  return result;
+};
